73c7b0f8ee928b07e54347ecabf36ac06ef9611c
KeigoHashimoto <kei.work.info@gmail.com>
2022-10-17 19:39:03 +0900

    fix

--- resources/views/commons/myBoard.blade.php
+++ resources/views/commons/myBoard.blade.php
@@ -32,17 +32,6 @@
                         {!! link_to_route('board.show',$board->title,[$board->id],['class'=>'board-title']) !!}
                     </div>
                     <p class="small">{{ $board->created_at }}</p>
-
-
-                    @if(\Auth::id() === 1)
-                        <div id="admin">admin btns</div>
-
-                        <div id="admin-btns">
-                            {{ Form::open(['route'=>['board.delete',$board->id],'method'=>'delete']) }}
-                                {{ Form::submit('削除',['class'=>'delete']) }}
-                            {{ Form::close() }}
-                        </div>
-                    @endif
                 </div>
             @endif
         @endforeach
