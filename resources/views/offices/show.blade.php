b2d57e6e94bddb1431128bd494dcb3b9f17b8781
KeigoHashimoto <kei.work.info@gmail.com>
2022-11-04 01:31:49 +0900

    fix

--- resources/views/offices/show.blade.php
+++ resources/views/offices/show.blade.php
@@ -58,12 +58,10 @@
 
             <div class="new-discussion" v-show="activeTab === ''" >
                 <h2>最新の議論</h2>
-                @foreach($users as $user)
-                    @foreach($user->boards()->where('office_id',$office->id)->orderBy('created_at','desc')->offset(0)->limit(2)->get() as $board)
-                        @if($board->office_id == $office->id)
-                            <h4 class="center">{!! link_to_route('board.show',$board->title,[$board->id]) !!}</h4>
-                        @endif
-                    @endforeach
+                @foreach($office->affiliationBoards->where('office_id',$office->id)->orderBy('created_at','desc')->offset(0)->limit(2)->get() as $board)
+                    @if($board->office_id == $office->id)
+                        <h4 class="center">{!! link_to_route('board.show',$board->title,[$board->id]) !!}</h4>
+                    @endif
                 @endforeach
             </div>
 
