1c2a4e37e3c0d85b7a4b758f7031af9b4bdc8415
KeigoHashimoto <kei.work.info@gmail.com>
2022-11-04 00:01:12 +0900

    fix

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -18,7 +18,7 @@
 
                     <!-- 自分以外の投稿 -->
                     <div class="opinion">
-                        <div v-if="user in users" :key="user.id">
+                        <div v-for="user in users" :key="user.id">
                             <div v-if="user.id === opinion.user_id" class="opinion-profile">
                                 <a :href="'/community-app/show/' + user.id"><img class="opinion-profile-img" :src="'/community-app/uploads/' + user.profile_img" alt=""></a>
                                 <p>{{ user.name }}</p>
