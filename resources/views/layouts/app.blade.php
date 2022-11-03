2fe01f919d49bf7b60ebd71a280fbed3c2e1702e
KeigoHashimoto <kei.work.info@gmail.com>
2022-11-03 22:42:06 +0900

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href={{ asset('/css/app.css') }} rel="stylesheet">
+    <link href={{ mix('/css/app.css') }} rel="stylesheet">
     <link rel="stylesheet" href={{ asset('/css/style.css') }}>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -25,6 +25,6 @@
 
     <script src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src={{ asset('/js/app.js') }}></script>
+    <script src={{ mix('/js/app.js') }}></script>
 </body>
 </html>
