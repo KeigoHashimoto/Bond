8c17b90d7b0b36f20b4aa4d538dc50b5dd78e27b
KeigoHashimoto <kei.work.info@gmail.com>
2022-12-20 00:59:16 +0900

    fix

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -12,13 +12,13 @@
                                 <div class="self-opinion-content">{{ opinion.opinion }}<br>
                                     <div  v-show="opinion.img_path != ''">
                                         <a :href="'/community-app/uploads/' + opinion.img_path">
-                                            <img :src="'community-app//uploads/' + opinion.img_path" alt="" class="opinion-img">
+                                            <img :src="'/community-app/uploads/' + opinion.img_path" alt="" class="opinion-img">
                                         </a>
                                     </div>
                                 </div>
     
                                 <div class="self-opinion-profile">
-                                    <a :href="'community-app//show/' + authUser.id">
+                                    <a :href="'/community-app/show/' + authUser.id">
                                         <img class="self-opinion-profile-img" :src="'community-app//uploads/' + authUser.profile_img" alt="">
                                     </a>
                                     <p>{{ authUser.name }}</p>
@@ -31,16 +31,16 @@
                         <div v-for="user in users" :key="user.id">
                             <div class="opinion">
                                 <div v-if="user.id === opinion.user_id" class="opinion-profile">
-                                    <a :href="'community-app//show/' + user.id">
-                                        <img class="opinion-profile-img" :src="'community-app//uploads/' + user.profile_img" alt="">
+                                    <a :href="'/community-app/show/' + user.id">
+                                        <img class="opinion-profile-img" :src="'/community-app/uploads/' + user.profile_img" alt="">
                                     </a>
                                     <p>{{ user.name }}</p>
                                 </div>
             
                                 <div class="opinion-content" v-if="opinion.user_id === user.id">{{ opinion.opinion }}<br>
                                     <div  v-show="opinion.img_path != ''">
-                                        <a :href="'community-app//uploads/' + opinion.img_path">
-                                            <img :src="'community-app//uploads/' + opinion.img_path" alt="" class="opinion-img">
+                                        <a :href="'/community-app/uploads/' + opinion.img_path">
+                                            <img :src="'/community-app/uploads/' + opinion.img_path" alt="" class="opinion-img">
                                         </a>
                                     </div>
                                 </div>
